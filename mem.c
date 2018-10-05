8fb325f6762ac94ee63dd12f5951d857ddde1ef4
ggg <g.liakopoulos@hotmail.com>
2018-10-05 03:07:41 +0200

    affiche la zone libre, dont la taille ne change pas pour le moment

--- mem.c
+++ mem.c
@@ -10,17 +10,17 @@ void* memoire;
 size_t taille_totale;
 
 void mem_init(char* mem, size_t taille){
-    	
-    if(memoire != NULL){
-        free(memoire);
-    }
-    struct fb *(*mem_fit_first_ptr)(struct fb*, size_t) = &mem_fit_first; //crétion prointeur de mem_fit_first
-    mem_fit(*mem_fit_first_ptr); // on défninie mem_fit_first comme recherche par defaut
-    mem = malloc(taille); //on alloue la plage mémoire
-    taille_totale = taille;
-    memoire = mem;        //On garde l'adresse de la plage memoire pour pouvoir eventuellement la liberer
-    tete =(struct fb *) mem; //on place la tête de liste en début de la plage mémoire
-    tete->size = taille;
+
+	if(memoire != NULL){
+		free(memoire);
+	}
+	struct fb *(*mem_fit_first_ptr)(struct fb*, size_t) = &mem_fit_first; //crétion prointeur de mem_fit_first
+	mem_fit(*mem_fit_first_ptr); // on défninie mem_fit_first comme recherche par defaut
+	mem = malloc(taille); //on alloue la plage mémoire
+	taille_totale = taille;
+	memoire = mem;        //On garde l'adresse de la plage memoire pour pouvoir eventuellement la liberer
+	tete =(struct fb *) mem; //on place la tête de liste en début de la plage mémoire
+	tete->size = taille;
 }
 
 //change la fonction de recherche
@@ -34,25 +34,25 @@ struct fb* mem_fit_first(struct fb* list, size_t size){
 	while(tete != NULL && tete->size < size + sizeof(struct fb)){
 		tete = tete->next;
 	}
-    return tete;
+	return tete;
 }
 
 //reçoit taille à allouer, retourne une adresse ou NULL
 void* mem_alloc(size_t taille){
-	
-	size_t tailleTotale = taille + sizeof(size_t); //taille totale = taille + 8 octets
-	struct fb* libre = g_fonction_defaut(tete,tailleTotale);
+
+	size_t taille_a_ecrire = taille + sizeof(size_t); //taille totale = taille + 8 octets
+	struct fb* libre = g_fonction_defaut(tete,taille_a_ecrire);
 	if(libre == NULL){
 		return NULL;
 	}
 	else{
-		size_t* ecriture = (size_t*)((char*)libre + (libre->size) - tailleTotale);//adresse écriture taille= adresse zone libre + taille zone libre - taille totale
-		ecriture[0] = tailleTotale;//écrit la taille
-		libre->size = libre->size - tailleTotale;
+		size_t* ecriture = (size_t*)( ((void*)libre) + libre->size - taille_a_ecrire);//adresse écriture taille= adresse zone libre + taille zone libre - taille totale à allouer
+		*ecriture = taille_a_ecrire;//écrit la taille
+		libre->size = libre->size - taille_a_ecrire;
 		return (void*) ecriture++;//retourne l'adresse = adresse écriture taille + 8 octets
 	}
-	
-	
+
+
 }
 
 size_t mem_get_size(void *zone){
@@ -63,17 +63,17 @@ void mem_free(void *zone){
 	struct fb *cp_tete = tete;
 	struct fb *z_avant = NULL;
 	struct fb *z_apres_next = NULL;
-	
-	while(cp_tete->next!=NULL){		
+
+	while(cp_tete->next!=NULL){
 		if(&cp_tete->next == (zone + (size_t)zone)){
 			z_apres_next = cp_tete;
-		}		
+		}
 		if ((&cp_tete + cp_tete->size)==zone){
 			z_avant = cp_tete;
 		}
 		cp_tete=cp_tete->next;
 	}
-	
+
 	if(z_avant==NULL && z_apres_next == NULL){
 		size_t temp = (size_t) zone;
 		cp_tete = tete;
@@ -94,30 +94,38 @@ void mem_free(void *zone){
 	else{
 		z_avant->size += (size_t) zone + z_apres_next->next->size;
 		z_apres_next->next = z_apres_next->next->next;
-		
+
 	}
-	
+
 }
 
 void mem_show(void (*print)(void *, size_t, int free)){
-    
-    struct fb* current = memoire;
-    
-    if(!((void*) current == memoire)){
-        printf("oups! mauvaise alignement current = %p memoire = %p", &current, memoire);}
+
+	struct fb* current = memoire;
+
+		//printf("oups! mauvaise alignement current = %p memoire = %p tete = %p", current, memoire, tete);
 
 	while((void *)current <= (void *)(memoire + taille_totale)){
-        struct fb* tete_test = tete;
-        while(tete_test != NULL){
-            if(&tete_test == &current){
-                print(current,current->size,1);
-            }
-        if(tete_test==NULL)
-            print(current,current->size,0);
-        tete_test = tete_test->next;
-        }
-        current = current + current->size;
-    }
+		struct fb* tete_test = tete;
+		int found = 0;
+		while(tete_test != NULL){
+			if(tete_test == current){
+				print(current,current->size,1);
+				found = 1;
+			}
+			tete_test = tete_test->next;
+		}
+		if(found == 0){
+			print(current,current->size,0);
+			found = 1;
+			void * temp = ((void*)current) + (size_t)current;
+			current = temp;
+		}
+		else{
+			current = current + current->size;
+		}
+
+	}
 }
 
 
